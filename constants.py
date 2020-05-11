05ea518107b3b6f0097e51907688196f31ebc4eb
Andres Toledo <andres_toledo90@hotmail.com>
2020-05-11 15:56:42 +0200

    Minor mod constants.py
    Added few lines to comment/uncomment, depending on runtime

--- constants.py
+++ constants.py
@@ -1,4 +1,11 @@
+# Run locally
 DATASETS = "Datasets/fma_small/"
 METADATA = "Datasets/fma_metadata/"
 MFCC = "Datasets/mfcc.csv"
 SPECTROGRAM = "Datasets/spectrogram.csv"
+
+# # Run on Google Colab
+# DATASETS = "/content/drive/My Drive/Datasets/fma_small/"
+# METADATA = "/content/drive/My Drive/Datasets/fma_metadata/"
+# MFCC = "/content/drive/My Drive/Datasets/mfcc.csv"
+# SPECTROGRAM = "/content/drive/My Drive/Datasets/spectrogram.csv"
