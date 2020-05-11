58ba3ddb17a597387bade73578a0b418c58ee65e
Andres Toledo <andres_toledo90@hotmail.com>
2020-05-11 20:58:40 +0200

    Testing

--- constants.py
+++ constants.py
@@ -2,3 +2,9 @@ DATASETS = "Datasets/fma_small/"
 METADATA = "Datasets/fma_metadata/"
 MFCC = "Datasets/mfcc.csv"
 SPECTROGRAM = "Datasets/spectrogram.csv"
+
+# Run on Google Colab
+# DATASETS = "/content/drive/My Drive/Datasets/fma_small/"
+# METADATA = "/content/drive/My Drive/Datasets/fma_metadata/"
+# MFCC = "/content/drive/My Drive/Datasets/mfcc.csv"
+# SPECTROGRAM = "/content/drive/My Drive/Datasets/spectrogram.csv"
