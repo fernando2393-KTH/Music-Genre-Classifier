bc171c6e1f1a821e6c34c51dc4958bc2971272b8
fernando2393 <garciasanzfernando2393@gmail.com>
2020-05-13 20:49:13 +0200

    ResNet architecture implemented.

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import Models.utils as utils
 import Models.conv1D_lstm as conv1
 import Models.conv2D as conv2
 import tensorflow as tf
-from tensorflow.keras.optimizers import Adam
+import Models.widenet28_10 as widenet
+from Models.wide_learning_rate import WideLearningRate
+from tensorflow.keras.optimizers import Adam, SGD
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.callbacks import ModelCheckpoint
 
@@ -17,30 +19,31 @@ def main():
     print(categories)
 
     # Instatiate the classifier model
-    #  # Uncomment for conv1
     # classifier = conv1.Classifier(layers=3,
     #                                  filters=[56, 56, 56],
     #                                  kernel_size=[5, 5, 5],
-    #                                  pool_size=[2, 2, 2])
-    # Uncomment for conv2
-    classifier = conv2.Classifier()
+    #                                  pool_size=[2, 2, 2])  # Uncomment for conv1
+    # classifier = conv2.Classifier()  # Uncomment for conv2
+    classifier = widenet.Classifier()  # Uncomment for widenet
     # Format input for this type of network
     x_train, x_val, x_test = classifier.data_format(x_train, x_val, x_test)
     # One-hot encoding of classes
     y_train, y_val, y_test = utils.targets_to_categorical(categories, y_train, y_test, y_val)
     # Training parameters
     epochs = 50  # Train for 30 epochs
-    lr = 0.001  # Initial learning rate
+    lr = 0.001  # Initial learning rate for Adam
     batch_size = 16
     tf.random.set_seed(1234)
     # Get the inputs placeholder
     inputs = tf.keras.Input(shape=x_train.shape[1:])
+    model = classifier.make_resnet_filter(inputs, depth=28, widen_factor=10)  # Uncomment for widenet
     # model = classifier.build(inputs, len(categories))  # Uncomment for conv1
-    model = classifier.build(inputs)  # Uncomment for conv2
+    # model = classifier.build(inputs)  # Uncomment for conv2
     print("Summary:")
     print(model.summary())
-
-    opt = Adam(learning_rate=lr)
+    # opt = Adam(learning_rate=lr)  # Uncomment for conv1 and conv2
+    opt = SGD(lr=WideLearningRate.get_learning_rate(epoch=0),
+              momentum=0.9, nesterov=True)  # Uncomment for widenet
     model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
     # Callbacks: early stopping and checkpoint
     early_stopping = EarlyStopping(monitor='val_accuracy', verbose=1,
